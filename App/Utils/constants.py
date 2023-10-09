4208c4cfb8403cfc507ce3cd8147760748256dcb
Arthur Rogado <arthurrogadoreis@hotmail.com>
2023-10-09 13:41:26 -0300

    update URL_WEBAPP constant

--- App/Utils/constants.py
+++ App/Utils/constants.py
@@ -3,3 +3,7 @@ CLOUD_ID = '-100' # New cloud group
 
 # Admin
 ADMIN_ID = ''
+
+URL_WEBAPP = "https://your_url#" # changes to your webapp url if you want to
+# Default routes in webapp
+URL_HOME = URL_WEBAPP + "home"
